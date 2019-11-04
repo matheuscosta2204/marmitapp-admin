38d1d4694ed70b0ea6056895f53487f820faddc4
matheuscosta2204 <matheuscosta2204@gmail.com>
2019-11-04 16:53:03 -0300

    Home with buttons

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 import './home.scss';
 import Toolbar from '../../ui/toolbar/toolbar';
+import GooglePlayLogo from '../../media/svg/google-play.svg';
+import AppStoreLogo from '../../media/svg/app-store.svg';
 
 const Home = () => {
     return (
         <Container className="home-container">
             <Toolbar />
-            <Row className="justify-content-center pt-8 home-title">
-                Partners
+            <Row className="flex-column align-items-center home-content">
+                <h1>Find your menu today!</h1>
+                <Link to="/partners" className="home-partners-buttom">
+                    <FontAwesomeIcon icon={faSearch} size="lg" />
+                    <h5>Our partners</h5>
+                </Link>
+            </Row>
+            <Row className="home-store-content">
+                <img src={GooglePlayLogo} alt="google play" width="50" />
+                <img src={AppStoreLogo} alt="app store" width="50" />
             </Row>
         </Container>
     )
